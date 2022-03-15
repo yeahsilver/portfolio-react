6c224f16f547099d0b1290516e4776a504477d71
yeahsilver <gjdpdms2005@gmail.com>
2022-03-16 01:02:26 +0900

    feat: experience 퍼블리싱

--- portfolio/src/components/experience/Experience.jsx
+++ portfolio/src/components/experience/Experience.jsx
@@ -1,9 +1,107 @@
 import React from 'react'
 import './experience.css'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
     return (
-        <section id="experience">Experience</section>
+        <section id="experience">Experience
+            <h5>What Skills I Have</h5> 
+            <h2> My Tech Stack </h2>
+
+            <div className="container experience__container">
+                <div className="experience__basic">
+                        <h3>Basic</h3>
+                        <div className="experience__content">
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Git</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Docker</h4>
+                            </div>
+                            
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Linux</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Vim</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>VS Code</h4>
+                            </div>
+                        </article>
+                        
+                    </div>
+                </div>
+
+                <div className="experience__programming">
+                    <h3>Programming</h3>
+
+                    <div className="experience__content">
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>C++</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Swift</h4>
+                            </div>
+                            
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>React</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>HTML & CSS</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Java</h4>
+                            </div>
+                        </article>
+
+                        <article className="experience__details">
+                            <BsPatchCheckFill className="experience__details-icon" />
+                            <div>
+                                <h4>Javascript</h4>
+                            </div>
+                        </article>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
