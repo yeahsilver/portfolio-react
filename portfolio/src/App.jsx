229e976a4c86b3a65375a224d164858cfe4ab087
yeahsilver <gjdpdms2005@gmail.com>
2022-03-18 17:51:47 +0900

    feat: 애니메이션 초기 설정

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -8,15 +8,94 @@ import Portfolio from './components/portfolio/Portfolio'
 import Activity from './components/activity/Activity'
 import Footer from './components/footer/Footer'
 
+import AOS from "aos"
+import "aos/dist/aos.css"
+
 const App = () => {
+
+    useEffect(() => {
+        AOS.init();
+    })
+
     return(
         <>
-        <Header/>
-        <About />
-        <Awards />
-        <Experience />
-        <Portfolio />
-        <Activity />
+        <div 
+           data-aos="slide-up"
+           data-aos-offset="100"
+           data-aos-delay="10"
+           data-aos-duration="500"
+           data-aos-mirror="true"
+           data-aos-once="false"
+           data-aos-anchor-placement="top-center"
+        >
+            <Header/>
+        </div>
+
+        <div 
+           data-aos="fade-up"
+           data-aos-offset="100"
+           data-aos-delay="10"
+           data-aos-duration="500"
+           data-aos-easing="ease-in-out"
+           data-aos-mirror="true"
+           data-aos-once="false"
+           data-aos-anchor-placement="top-center"
+        >
+            <About />
+        </div>
+
+        <div 
+           data-aos="slide-left"
+           data-aos-offset="200"
+           data-aos-delay="10"
+           data-aos-duration="1000"
+           data-aos-easing="ease-in-out"
+           data-aos-mirror="true"
+           data-aos-once="false"
+           data-aos-anchor-placement="top-center"
+        >
+            <Awards />
+        </div>
+
+        <div 
+           data-aos="fade-up"
+           data-aos-offset="200"
+           data-aos-delay="10"
+           data-aos-duration="500"
+           data-aos-easing="ease-in-out"
+           data-aos-mirror="true"
+           data-aos-once="false"
+           data-aos-anchor-placement="top-center"
+        >
+            <Experience />
+        </div>
+
+        <div 
+           data-aos="fade-up"
+           data-aos-offset="200"
+           data-aos-delay="10"
+           data-aos-duration="500"
+           data-aos-easing="ease-in-out"
+           data-aos-mirror="true"
+           data-aos-once="false"
+           data-aos-anchor-placement="top-center"
+        >
+            <Portfolio />
+        </div>
+
+        <div 
+           data-aos="fade-up"
+           data-aos-offset="200"
+           data-aos-delay="0"
+           data-aos-duration="500"
+           data-aos-easing="ease-in-out"
+           data-aos-mirror="true"
+           data-aos-once="false"
+           data-aos-anchor-placement="top-center"
+        >
+            <Activity />
+        </div>
+
         <Footer />
         </>
     )
