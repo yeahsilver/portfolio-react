6544ceae6236e469626c96e842c1bfd52fee7a8b
yeahsilver <gjdpdms2005@gmail.com>
2022-03-28 00:09:28 +0900

    feat: 이메일 버튼 추가

--- portfolio/src/components/header/ContactButton.jsx
+++ portfolio/src/components/header/ContactButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BsLinkedin } from 'react-icons/bs';
 import { FaGithub } from 'react-icons/fa';
-import { SiNaver } from 'react-icons/si';
+import { SiNaver, SiGmail } from 'react-icons/si';
 import './header.css'
 
 const ContactButton = () => {
@@ -10,6 +10,7 @@ const ContactButton = () => {
             <a href="https://www.linkedin.com/in/yeeun-heo-23a722201/"><BsLinkedin /></a>
             <a href="https://github.com/yeahsilver"><FaGithub /></a>
             <a href="https://blog.naver.com/0_0yeggy"><SiNaver /></a>
+            <a href="mailto:yeeun.dev@gmail.com"><SiGmail /></a>
         </div>
     )
 }
